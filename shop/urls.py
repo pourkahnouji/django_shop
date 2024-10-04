cbdc7d7bcb156da96212c01c93ab4b2ea82947ed
hakankahnoji <mohammadaminpoorkahnoji@gmail.com>
2024-10-04 11:24:05 +0330

    fix bug in list and detail

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('products/', views.products_list, name='product-list'),
+    path('products/', views.products_list, name='product_list'),
     path('products/<slug:category_slug>/', views.products_list, name='product_list_by_category'),
     path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
