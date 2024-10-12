127a083bb32b89a69f3bb78508769f8f173e4c02
hakankahnoji <mohammadaminpoorkahnoji@gmail.com>
2024-10-12 12:23:01 +0330

    modified add section in cart app

--- cart/cart.py
+++ cart/cart.py
@@ -4,7 +4,7 @@ from shop.models import Product
 class Cart:
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get['cart']
+        cart = self.session.get('cart')
 
         if not cart:
             cart = self.session['cart'] = {}
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from shop.models import Product
+from django.views.decorators.http import require_POST
+from .cart import Cart
+from django.http import JsonResponse
+
 
 # Create your views here.
+@require_POST
+def add_to_cart(request, product_id):
+    try:
+        product = get_object_or_404(Product, id=product_id)
+        cart = Cart(request)
+        cart.add(product)
+        context = {
+            'item_count': len(cart),
+            'total_price': cart.get_total_price()
+        }
+        return JsonResponse(context)
+    except:
+        return JsonResponse('error')
